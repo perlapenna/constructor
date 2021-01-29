8b153c89336ed015758b17a5b8210b15cdca4a08
perlapenna <70943811+perlapenna@users.noreply.github.com>
2021-01-29 16:06:29 +0300

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,81 @@
+ $(window).on('load', function () {
+ 	$('.preloader').hide();
+
+	var $base = "iPhone 12 Pro Max";
+	var $model = "Minimalistic Plate";
+	var $basePrice = "1490";
+	var $leather = $(".color-radio:checked").attr("dmn-name");
+	var $plate = $(".plate-radio:checked").attr("dmn-name");
+	var $engraving = $("#engraving-container").text();
+	var $font = $(".font-radio:checked").attr("dmn-font");
+
+	$(".model-vendor").text($base);
+	$(".model-name").text($model);
+	$(".title-cost, .btn-cost").text($basePrice);
+	var $total = $(".title-cost").text();
+
+    $('#dev-engrave').on('input', function() {
+    	var $val = $(this).val()
+    	$("#engraving-container").text($val);
+    	var $engraveLength = $("#engraving-container").text().length;
+		if (($engraveLength >= 1) && ($engraveLength < 8)) {
+        	$("#engraving-container").css({"font-size" : "1.25rem", "top" : "2px"});
+    	}
+    	else if (($engraveLength > 6) && ($engraveLength < 12)) {
+    		$("#engraving-container").css({"font-size" : "1.125rem", "top" : "2px"});
+    	}
+    	else if (($engraveLength > 12) && ($engraveLength < 19)) {
+    		$("#engraving-container").css({"font-size" : ".9rem", "top" : "5px"});
+    	}
+    	else if (($engraveLength < 1)) {
+    		$("#engraving-container").text("Надпись");
+    		$val = $("#engraving-container").text();
+    	}
+    	$engraving = $val;
+	});
+
+	$(".color-radio").click(function() {
+   		if($(".color-radio").is(":checked")) {
+   			$("#selected-color").text($(this).attr("dmn-name"));
+   			$(".leather-layer").attr("src", "assets/img/12pm-leather-calfskin-"+$(this).attr("dmn-color")+".png");
+   			$leather = $(this).attr("dmn-name");
+   		}
+	});
+
+	$(".plate-radio").click(function() {
+   		if($(".plate-radio").is(":checked")) {
+   			$("#selected-plate").text($(this).attr("dmn-name"));
+   			$(".plate-container").attr("src", "assets/img/plate-"+$(this).attr("dmn-color")+".png");
+   			$(".title-cost, .btn-cost").text(parseInt($basePrice)+parseInt($(this).attr("dmn-price")));
+   			$total = $(".title-cost").text();
+   			$plate = $(this).attr("dmn-name");
+   		}
+	});
+
+	$(".font-radio").click(function() {
+   		if($(".font-radio").is(":checked")) {
+   			$("#selected-font").text($(this).attr("dmn-name"));
+   			$("#engraving-container").css({"font-family" : $(this).attr("dmn-font")});
+   			$font = $(this).attr("dmn-font");
+   		}
+	});
+
+	function updateJson() {
+		caseObject = {
+			base: $base,
+			model: $model,
+			leather: $leather,
+			plate: $plate,
+			engraving: $engraving,
+			font: $font,
+			total: $total
+		}
+		return caseObject;
+	}
+
+	$(".btn-buy").click(function() {
+		$(".objectContainer").text(JSON.stringify(updateJson()));
+	});
+
+
+ });
